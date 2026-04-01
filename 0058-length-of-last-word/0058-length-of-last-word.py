1baebf07b2812c01deb9f02fb4afb0a0da5fb44e
Arsathmuha <arsath31105@gmail.com>
2026-04-01 10:53:22 +0530

    Time: 2 ms (4.55%), Space: 19.4 MB (36.14%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,12 +1,12 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s =  s.rstrip()
-        ans=""
+        s = s.rstrip()
+        ans = ""
+
         for ch in s:
-            if ch==" ":
-                ans=""
-            ans+=ch
-            ans=ans.strip()
+            if ch == " ":
+                ans = ""
+            else:
+                ans += ch
 
         return len(ans)
-        
