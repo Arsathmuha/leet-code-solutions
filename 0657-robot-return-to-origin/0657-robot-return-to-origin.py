abc175c16b5e420030cc643993f48f92cd81ef64
Arsathmuha <arsath31105@gmail.com>
2026-04-11 11:55:58 +0530

    Time: 16 ms (32.52%), Space: 19.5 MB (19.35%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,14 +1,12 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        h = ""
-        l = ""
-
+        h=""
+        l=""
         for ch in moves:
-            if ch == 'R' or ch == 'L':
+            if ch =='R' or ch== 'L':
                 l += ch
-            elif ch == 'U' or ch == 'D':
+            elif ch =='U' or ch== 'D':
                 h += ch
-
         if l.count('R') == l.count('L') and h.count('U') == h.count('D'):
             return True
         else:
