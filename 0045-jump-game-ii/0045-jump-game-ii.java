282b0fcb162cb47baf8e741532ab7f2c831b56e6
Arsathmuha <arsath31105@gmail.com>
2026-05-24 20:00:07 +0530

    Time: 1 ms (99.58%), Space: 47.2 MB (74.08%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int jump(int[] nums) {
+
+        int jumps = 0;
+
+        int currentEnd = 0;
+
+        int farthest = 0;
+
+        // no need to process last index
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            farthest =
+                    Math.max(farthest,
+                             i + nums[i]);
+
+            // end of current jump range
+            if (i == currentEnd) {
+
+                jumps++;
+
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
